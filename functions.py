7bf172c002627a50d6188ffbd653547329eead18
Furtif Coder <ode808prod@gmail.com>
2023-02-09 00:01:38 +0100

    Functions toBinary & toDecimal completed

--- functions.py
+++ functions.py
@@ -2,12 +2,27 @@
 
 # convert a decimal byte to binary
 def toBinary(decimalNumber):
+
+    # checking errors
+    try:
+        number = int(decimalNumber)
+        if number < 0 or number > 255:
+            return 1 # Out of range
+        else:
+            decimalNumber = number
+        
+    except ValueError:
+        return 2 # Extra characters
+    
+    
+
     power = 7
     binaryNumber = ""
-    while power > 0:
+    while power >= 0:
         weight = 2 ** power
         if decimalNumber >= weight:
             binaryNumber += "1"
+            decimalNumber -= weight
         else:
             binaryNumber += "0"
         power -= 1
@@ -15,7 +30,37 @@ def toBinary(decimalNumber):
 
 # convert a binary byte to decimal
 def toDecimal(binaryNumber):
-    pass
+    # checking errors
+    try:
+        if int(binaryNumber) < 0:
+            return 1 # Negative number
+        elif len(binaryNumber) > 8:
+            return 2 # more than 8 bits
+        else:
+            for digit in binaryNumber:
+                if int(digit) > 1:
+                    return 3 # Digit other than '0' or '1'       
+        
+    except ValueError:
+        return 4 # extra characters
+    
+    # Check missing bits and fill them with with '0'    
+    if len(binaryNumber) < 8:
+        nDigitsOff = 8 - len(binaryNumber)
+        while nDigitsOff > 0:
+            binaryNumber = "0" + binaryNumber
+            nDigitsOff -= 1
+        print(binaryNumber)
+    
+
+
+    decimalNumber = 0
+    power = 0
+    while power < 8:
+        weight = 2 ** power
+        decimalNumber += weight * int(binaryNumber[7-power])
+        power += 1
+    return decimalNumber
 
 # convert a decimal IP to binary
 def ipToBinary():
--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import messagebox
-
 import ttkbootstrap as tb
 from functions import *
 
@@ -11,32 +10,76 @@ root.geometry("500x320")
 root.maxsize(width = 500, height = 320)
 root.minsize(width = 500, height = 320)
 
-# functions command²
+# functions command
 def SelectOption(option):
-    print(f"{option}")
-    
+    #print(f"{option}")
+    pass
 
-def Calcul():
+# Option 1: decimal to binary
+def ToBinary(userInput):
+    converted = toBinary(userInput)
+    if converted == 1:
+        messagebox.showerror("Error!", f"{userInput} is out of range [0,255]")
+    elif converted == 2:
+        messagebox.showerror("Error!", f"{userInput} contains unexpected alphanumeric characters")
+    else:
+        rep = messagebox.showinfo(title="Conversion", message=f"{userInput} in binary is: {converted}")
+
+# Option 2: binary to decimal    
+def ToDecimal(userInput):
+    converted = toDecimal(userInput)
+    if converted == 1: # Negative number
+        messagebox.showerror("Error!", f"{userInput} is negative") 
+    elif converted == 2: # More than 8 bits
+        messagebox.showerror("Error!", f"You wrote: {userInput}\nMax length of a byte is 8 digits") 
+    elif converted == 3: # Digit other than '0' or '1'
+        messagebox.showerror("Error!", f"You wrote: {userInput}\nBinary digits only consist of '0' and '1'") 
+    elif converted == 4: # Extra characters
+        messagebox.showerror("Error!", f"'{userInput}' contains unexpected alphanumeric characters") 
+    else:
+        rep = messagebox.showinfo(title="Conversion", message=f"{userInput} in Decimal is: {converted}")
+
+
+
+
+def Action():
     userInput = entry.get()
-    rep = messagebox.showinfo(title="Conversion", message=f"You typed {userInput}")
-    print(rep)
-    entry.config(text = "")
-    
-# create a menu for conversion type
-conversionType = tb.Menubutton(root, bootstyle = "primary outlined", text = "Conversion Type")
+    option = 2
+    if len(userInput) == 0:
+            messagebox.showerror("Empty entry", f"{userInput} Please write something") # Negative number
+    else:
+        if option == 1:
+            ToBinary(userInput)
+        elif option == 2:
+            ToDecimal(userInput)
+            
+
+
+   
+
+# options of the menu
+options = ["Decimal to Binary", "Binary to Decimal", "IP Decimal to Binary", "Decode a.b.c.d/Mask"]
+
+# create a menubutton for conversion type
+conversionType = tb.Menubutton(root, bootstyle = "primary outlined", text="Conversion Type")
 conversionType.pack(pady = 40)
 
-# conversion menue
-conversionMenu = tb.Menu(conversionType)
-# options of the menue
-optionVar = tk.StringVar()
-id = 0
-options = ["Decimal to Binary", "Binary to Decimal", "IP Decimal to Binary", "Decode a.b.c.d/Mask"]
-for option in options:
-    conversionMenu.add_radiobutton(label = option, variable = optionVar, command = SelectOption)
+# create a menu
+menu = tb.Menu(conversionType)
 
-# Associate the options menu with the menu type
-conversionType['menu'] = conversionMenu
+# add option to menu
+optionVar = tk.StringVar()
+for option in options:
+    menu.add_radiobutton(label=option)
+
+
+
+
+# conversion menu
+#conversionMenu = tb.Menu(conversionType)
+
+
+
 
 # Create an entry field
 userInput = tk.StringVar()
@@ -45,8 +88,8 @@ entry.pack(pady = 50)
 
 # button
 validate = tb.Button(root, text = "Validate",
-                     command = lambda: Calcul(),
-                     bootstyle = "info")
-validate.pack(pady = 20)
+                     command = lambda: Action(),
+                     bootstyle = "secondary outline")
+validate.pack(pady = 10)
 
 root.mainloop()
