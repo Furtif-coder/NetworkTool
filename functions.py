00f0f41786f3153a0193c6fcce296b4a7d076ac2
Furtif Coder <ode808prod@gmail.com>
2023-12-26 22:14:26 +0100

    Basic set up to start

---
+++ functions.py
@@ -0,0 +1,12 @@
+# FUNCTIONS
+
+# convert a decimal byte to binary
+def toBinary():
+    pass
+
+# convert a binary byte to decimal
+def toDecimal():
+    pass
+
+
+
---
+++ main.py
@@ -0,0 +1,15 @@
+import tkinter
+import ttkbootstrap as tb
+
+root = tb.Window(themename = "superhero")
+root.title("Network Tool by @furtif_coder")
+root.geometry("500x320")
+root.maxsize(width = 500, height = 320)
+root.minsize(width = 500, height = 320)
+
+
+
+
+
+
+root.mainloop()
