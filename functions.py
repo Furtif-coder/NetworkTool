917bfa17cd25f6628bd7989e59133b1da2104d84
Furtif Coder <ode808prod@gmail.com>
2023-12-28 02:11:11 +0100

    Version 1.0.1: working but not checking errors for Option 3 & 4

--- functions.py
+++ functions.py
@@ -63,9 +63,125 @@ def toDecimal(binaryNumber):
     return decimalNumber
 
 # convert a decimal IP to binary
-def ipToBinary():
-    pass
+def ipToBinary(Ip):
+    try:
+        a, b, c, d = Ip.split(".")
+    except ValueError:
+        return -1
+    
+    ipBinary = f"{toBinary(int(a))}.{toBinary(int(b))}.{toBinary(int(c))}.{toBinary(int(d))}"
+    return ipBinary
 
 # convert a binary IP in to decimal
-def ipToDecimal():
-    pass
+# def ipToDecimal():
+# recieves a.b.c.d IP in base 2 and returns a decimal version of the IP
+def ipToDecimal(ipBinary):
+	a, b, c, d = ipBinary.split('.')
+
+	IP = f"{toDecimal(a)}.{toDecimal(b)}.{toDecimal(c)}.{toDecimal(d)}"
+	return IP
+   
+
+# From last program
+# Read the mask id 
+def readMask(num):
+	# Reading to Mask IP in base 2
+	r = int(num) % 8
+	n_fullbytes = int(num) // 8
+	n_emptbytes = 4 - n_fullbytes
+	
+	# The Mask IP in 32bits format
+	if r == 0:
+		maskBin32 =  "11111111"*n_fullbytes + "00000000"*n_emptbytes
+	else:
+		maskBin32 =  "11111111"*n_fullbytes + "1"*r + "0"*(8-r) + "00000000"*(n_emptbytes-1)
+	
+	return maskBin32
+
+# Operation AND between Host and Mask to find the Network Adress
+def calculateNetBin32(hostBin32, maskBin32):
+	i = 0
+	netBin32 = ""
+	while i < 32:
+		if int(hostBin32[i]) * int(maskBin32[i]) == 0:
+			netBin32 += "0"
+		else:
+			netBin32 += "1"	
+		i += 1
+	return netBin32
+
+# decode IP/mask
+def readFullIP(fullIP):
+    try:
+        a, b, c, d = fullIP.split(".")
+        d, maskID = d.split("/")
+        hostDecimal = f"{a}.{b}.{c}.{d}"
+    except ValueError:
+        return -1
+    
+    hostBin32 = toBinary(a) + toBinary(b) + toBinary(c) + toBinary(d) # Host IP in 32 bits format - used for calculation
+    hostBin = f"{toBinary(int(a))}.{toBinary(int(b))}.{toBinary(int(c))}.{toBinary(int(d))}" # Host IP in binary bytes
+    # Reading the 32bits format of a mask IP from a integer || ID_mask --> func: numToIP --> mask in 32bits 
+    maskBin32 = readMask(maskID)
+    maskBin = Bin32ToBinBytes(maskBin32)
+    maskDecimal = ipToDecimal(maskBin)
+
+    # Calculation of the network IP --> function "netIP_Bin" --> returns a.b.c.d in binary
+    netBin32 = calculateNetBin32(hostBin32, maskBin32)
+    netBin = Bin32ToBinBytes(netBin32)
+    netDecimal = ipToDecimal(netBin) # Network Adress in decimal bytes : a.b.c.d
+
+    return hostDecimal, maskDecimal, netDecimal
+
+def Read_full_IP(decIP_maskID):
+	a, b, c, d = decIP_maskID.split('.')
+	d, ID_mask = d.split('/')
+	IP_host_Dec = a + '.' + b + '.' + c + '.' + d
+
+	# All Adresses count
+	n_adresses = 2**(32-int(ID_mask))
+
+	# Reading the Host IP in base 2 / for calculation later
+	a = toBinary(int(a))
+	b = toBinary(int(b))
+	c = toBinary(int(c))
+	d = toBinary(int(d))
+
+	IP_host_Bin = a + "." + b + "." + c + "." + d # Host IP in binary bytes
+	IP_host_Bin32 = a + b + c + d # Host IP in 32 bits format - used for calculation
+	
+	# Reading the 32bits format of a mask IP from a integer || ID_mask --> func: numToIP --> mask in 32bits 
+	maskBin32 = readMask(ID_mask)
+	print(maskBin32)
+	# The Mask IP in Binary format in bytes: a.b.c.d
+	maskBin = Bin32ToBinBytes(maskBin32) # Mask IP in binary format
+	# Mask in decimal a.b.c.d format
+	maskDec = ipToBinary(maskBin)
+
+	
+	# Calculation of the network IP --> function "netIP_Bin" --> returns a.b.c.d in binary
+	IP_net_Bin32 = netBin32(IP_host_Bin32, maskBin32)
+	IP_net_Bin = Bin32ToBinBytes(IP_net_Bin32)
+	IP_net_Dec = ipToDecimal(IP_net_Bin) # Network Adress in decimal bytes : a.b.c.d
+
+	return hostDec, maskDec, netDec, n_adresses
+
+# Function that turns all the 32 bits of an IP adress to the usual a bytes (a.b.c.d) / Binary Version
+def Bin32ToBinBytes(IP_Bin32):
+	a = IP_Bin32[0:8]
+	b = IP_Bin32[8:16]
+	c = IP_Bin32[16:24]
+	d = IP_Bin32[24:32]
+
+	return a + '.' + b + '.' + c + '.' + d
+
+
+
+def Num_Adresses(n):
+	return 2**n
+
+def eNumBin(n):
+	i=0
+	for i in range(n):
+		print(toBinary(i))
+		i+=1
--- main.py
+++ main.py
@@ -10,10 +10,19 @@ root.geometry("500x320")
 root.maxsize(width = 500, height = 320)
 root.minsize(width = 500, height = 320)
 
+operation = ""
 # functions command
 def SelectOption(option):
+    global operation
     #print(f"{option}")
-    pass
+    if option == "Decimal to Binary":
+        operation = 1
+    elif option == "Binary to Decimal":
+        operation = 2
+    elif option == "Decimal IP to Binary":
+        operation = 3
+    elif option == "Decode a.b.c.d/Mask":
+        operation = 4
 
 # Option 1: decimal to binary
 def ToBinary(userInput):
@@ -39,29 +48,42 @@ def ToDecimal(userInput):
     else:
         rep = messagebox.showinfo(title="Conversion", message=f"{userInput} in Decimal is: {converted}")
 
+# Option 3: full IP decimal to binary
+def IpToBinary(userInput):
+    converted = ipToBinary(userInput)
+    if converted == -1:
+        messagebox.showerror("Error!", "Write an IP with decimal bytes: a.b.c.d")
+    else:
+        messagebox.showinfo("Conversion full IP", f"{userInput} : {converted}")
 
+# Option 4: decode a.b.c.d/mask
+def DecodeIpMask(userInput):
+    host, mask, network= readFullIP(userInput)
+    message = f"Host : {host}\nSubnet Mask: {mask}\nNetwork: {network}\n"
+    messagebox.showinfo(userInput, message)
 
 
 def Action():
     userInput = entry.get()
-    option = 2
+
     if len(userInput) == 0:
-            messagebox.showerror("Empty entry", f"{userInput} Please write something") # Negative number
+            messagebox.showerror("Empty entry", "Please write something") # Negative number
     else:
-        if option == 1:
+        if operation == 1:
             ToBinary(userInput)
-        elif option == 2:
+        elif operation == 2:
             ToDecimal(userInput)
-            
-
-
-   
-
+        elif operation == 3:
+            IpToBinary(userInput)
+        elif operation == 4:
+            DecodeIpMask(userInput)
+        else:
+            messagebox.showinfo("Conversion", "Choose the type of conversion in the menu on top") 
 # options of the menu
-options = ["Decimal to Binary", "Binary to Decimal", "IP Decimal to Binary", "Decode a.b.c.d/Mask"]
+options = ["Decimal to Binary", "Binary to Decimal", "Decimal IP to Binary", "Decode a.b.c.d/Mask"]
 
 # create a menubutton for conversion type
-conversionType = tb.Menubutton(root, bootstyle = "primary outlined", text="Conversion Type")
+conversionType = tb.Menubutton(root, bootstyle = "success outlined", text="Conversion Type")
 conversionType.pack(pady = 40)
 
 # create a menu
@@ -70,16 +92,10 @@ menu = tb.Menu(conversionType)
 # add option to menu
 optionVar = tk.StringVar()
 for option in options:
-    menu.add_radiobutton(label=option)
+    menu.add_radiobutton(label=option, command = lambda option = option: SelectOption(option))
 
-
-
-
-# conversion menu
-#conversionMenu = tb.Menu(conversionType)
-
-
-
+# associate the menu with the menu_button
+conversionType['menu'] = menu
 
 # Create an entry field
 userInput = tk.StringVar()
@@ -87,9 +103,7 @@ entry = tb.Entry(root)
 entry.pack(pady = 50)
 
 # button
-validate = tb.Button(root, text = "Validate",
-                     command = lambda: Action(),
-                     bootstyle = "secondary outline")
+validate = tb.Button(root, text = "Validate", command = lambda: Action(), bootstyle = "warning outline")
 validate.pack(pady = 10)
 
 root.mainloop()
